2ed05a5f857d889c5b50307755bf653207cec267
Scrirock <73163680+Scrirock@users.noreply.github.com>
2021-01-29 13:26:30 +0100

    hg

--- index.php
+++ index.php
@@ -6,6 +6,9 @@
  */
 // TODO Votre code ici.
 
+$lorem = file_get_contents("lire.txt");
+echo "<div>".$lorem."</div><br><br>";
+
 /**
  * 3. Faites la même chose à l'aide d'une boucle et de la fonction fgets(), récupérez les lignes du fichier une à une.
  * 4. Affichez le contenu de chaque ligne dans un nouveau span.
@@ -13,6 +16,13 @@
  */
 // TODO Votre code ici.
 
+$file = fopen("lire.txt", 'rb');
+while ($ligne = fgets($file)){
+    echo "<span>".nl2br($ligne)."</span>";
+}
+fclose($file);
+
+echo "<br><br>";
 
 /**
  * 5. Récupérez les caractères un à un et n'affichez le caractère que s'il s'agit d'autre chose qu'un espace, qu'un point ou qu'un point d'interrogation / exclamation.
@@ -20,6 +30,12 @@
  */
 // TODO Votre code ici.
 
+$file = fopen("lire.txt", 'rb');
+while ($char = fgetc($file)){
+    if ($char === " " || $char === "." || $char === "," || $char === "!" || $char === "?") echo "";
+    else echo $char;
+}
+fclose($file);
 
 /**
  * 6. Ne cherchez pas la logique dans cet exo ^^
